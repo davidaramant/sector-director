36b4835fbe760b64080aeffd0909c2ac90478716
David Aramant <davidaramant@gmail.com>
2018-07-27 21:39:36 -0400

    Port of metadata classes

---
+++ src/Metadata/NamedItem.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2016, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+namespace SectorDirector.Metadata
+{
+    public class NamedItem
+    {
+        public NamedItem(string formatName, string className)
+        {
+            FormatName = formatName;
+            ClassName = className;
+        }
+
+        public string FormatName { get; }
+        public string ClassName { get; }
+    }
+}
