18b189247c56f474819f3551ef40397d88a4452a
David Aramant <davidaramant@gmail.com>
2018-07-27 20:57:15 -0400

    Empty projects

---
+++ src/TestLauncher/Program.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2018, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+
+namespace SectorDirector.TestLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
