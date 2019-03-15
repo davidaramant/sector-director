f9c793cf7f5f3a2a07b043e21caa6149193dac9d
David Aramant <davidaramant@gmail.com>
2019-03-15 15:13:27 -0400

    Put in example grammar

--- src/DataModelGenerator/Program.cs
+++ src/DataModelGenerator/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2019, David Aramant
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
 
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -19,6 +21,15 @@ namespace SectorDirector.DataModelGenerator
             {
                 UdmfParserGenerator.WriteTo(parserStream);
             }
+
+            // Generate HIME grammar
+            Process.Start(
+                Path.Combine(solutionBasePath, "..", "hime", "himecc.bat"), 
+                "Udmf.gram");
+
+            // TODO: Copy artifacts to source directory
+
+            Console.ReadLine();
         }
     }
 }
