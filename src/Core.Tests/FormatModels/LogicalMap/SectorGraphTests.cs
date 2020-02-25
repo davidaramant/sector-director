d40dbc4e5ea4726aff3f195f3af05eab9140acf7
David Aramant <davidaramant@gmail.com>
2020-02-25 10:19:15 -0500

    Another failing test for the case with a hole in it

--- src/Core.Tests/FormatModels/LogicalMap/SectorGraphTests.cs
+++ src/Core.Tests/FormatModels/LogicalMap/SectorGraphTests.cs
@@ -328,5 +328,167 @@ namespace SectorDirector.Core.Tests.FormatModels.LogicalMap
             Assert.That(graph.LogicalSectors, Has.Count.EqualTo(1));
             Assert.That(graph.LogicalSectors.First(), Has.Count.EqualTo(2));
         }
+
+        [Test]
+        [Explicit("Failing test!")]
+        public void ShouldGenerateSubSectorWithHoleWhenSectorHasHole()
+        {
+            // *--------*
+            // |        |
+            // |  *--*  |
+            // |  |  |  |
+            // |  *--*  |
+            // |        |
+            // *--------*
+
+            var map = new MapData
+            {
+                NameSpace = "Doom",
+                Vertices =
+                {
+                    new Vertex // 0
+                    {
+                        X = 0,
+                        Y = 0,
+                    },
+                    new Vertex // 1
+                    {
+                        X = 1000,
+                        Y = 0,
+                    },
+                    new Vertex // 2
+                    {
+                        X = 1000,
+                        Y = 1000,
+                    },
+                    new Vertex // 3
+                    {
+                        X = 0,
+                        Y = 1000,
+                    },
+                    new Vertex // 4
+                    {
+                        X = 250,
+                        Y = 250,
+                    },
+                    new Vertex // 5
+                    {
+                        X = 750,
+                        Y = 250,
+                    },
+                    new Vertex // 6
+                    {
+                        X = 750,
+                        Y = 750,
+                    },
+                    new Vertex // 7
+                    {
+                        X = 250,
+                        Y = 750,
+                    },
+                },
+                LineDefs =
+                {
+                    new LineDef // 0
+                    {
+                        V1 = 0,
+                        V2 = 1,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 1
+                    {
+                        V1 = 1,
+                        V2 = 2,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 2
+                    {
+                        V1 = 2,
+                        V2 = 3,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 3
+                    {
+                        V1 = 3,
+                        V2 = 0,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 4
+                    {
+                        V1 = 4,
+                        V2 = 7,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 5
+                    {
+                        V1 = 7,
+                        V2 = 6,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 6
+                    {
+                        V1 = 6,
+                        V2 = 5,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                    new LineDef // 7
+                    {
+                        V1 = 5,
+                        V2 = 4,
+                        SideFront = 0,
+                        Blocking = true,
+                    },
+                },
+                SideDefs =
+                {
+                    new SideDef // 0
+                    {
+                        Sector = 0,
+                        TextureMiddle = "STARTAN1",
+                    },
+                },
+                Sectors =
+                {
+                    new Sector
+                    {
+                        HeightFloor = 0,
+                        HeightCeiling = 128,
+                        TextureFloor = "FLOOR0_1",
+                        TextureCeiling = "CEIL1_1",
+                        LightLevel = 192,
+                    },
+                },
+                Things =
+                {
+                    new Thing
+                    {
+                        X = 100,
+                        Y = 100,
+                        Angle = 90,
+                        Type = 1,
+                        Skill1 = true,
+                        Skill2 = true,
+                        Skill3 = true,
+                        Skill4 = true,
+                        Skill5 = true,
+                        Single = true,
+                        Dm = true,
+                        Coop = true,
+                    }
+                }
+            };
+
+            var graph = SectorGraph.BuildFrom(map);
+
+            Assert.That(graph.LogicalSectors, Has.Count.EqualTo(1));
+            Assert.That(graph.LogicalSectors.First(), Has.Count.EqualTo(1));
+        }
     }
 }
